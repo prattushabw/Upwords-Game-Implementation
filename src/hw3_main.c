a2d5c3668c0cd3af8eab2ba6529f55add70845d1
Prattusha <56740350+prattushabw@users.noreply.github.com>
2024-03-20 02:29:16 +0000

    push

--- src/hw3_main.c
+++ src/hw3_main.c
@@ -3,7 +3,51 @@
 
 #include "hw3.h"
 
-int main(void) {
-    // Add your own code here
-    return 0;
+void print_board(GameState *game) {
+    if (game == NULL || game->board == NULL) {
+        printf("The game or game board is not initialized.\n");
+        return;
+    }
+
+    for (int i = 0; i < game->rows; i++) {
+        for (int j = 0; j < game->cols; j++) {
+            if (game->board[i][j].top != NULL) {
+                printf("%c ", *game->board[i][j].top);
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n"); 
+
+    }
+
+    for (int i = 0; i < game->rows; i++) {
+        for (int j = 0; j < game->cols; j++) {
+                printf("%i ", game->board[i][j].height);
+        }
+        
+        printf("\n"); 
+    }
 }
+
+
+int main(void) {
+    int num_tiles_placed=0;
+   GameState *game = initialize_game_state("./tests/boards/board01.txt"); 
+    if (!game) {
+        fprintf(stderr, "Failed to initialize game.\n");
+        return EXIT_FAILURE;
+    }
+    print_board(game);
+    printf("\n\n");
+ 
+    game = place_tiles(game, 3, 2, 'H', "C", &num_tiles_placed);
+    print_board(game);
+
+    save_game_state(game, "./tests/actual_outputs/output.txt");
+    printf("TilesPlaced: %d\n", num_tiles_placed);
+    free_game_state(game);
+
+    return EXIT_SUCCESS;
+}
+  
